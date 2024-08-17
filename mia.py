ece14b4366134e9f66df27a5b8b9ff7a80eab3f8
vfusaro <vfusaro@gmail.com>
2024-08-17 15:55:21 -0700

    import env file in mia.py

--- mia.py
+++ mia.py
@@ -2,6 +2,11 @@ from flask import Flask
 import os
 from config import Config, DevConfig, TestConfig, ProductionConfig
 from app import create_app
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = os.path.join(basedir, '.miaenv')
+load_dotenv(dotenv_path)
 
 # Determine which configuration to use based on the FLASK_ENV environment variable
 env_config = os.getenv('FLASK_ENV', 'local')  # Default to 'local'
