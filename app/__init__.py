f48411a76e8ece32a6d305cc0add355df438649a
IvantheDugtrio <idedios@usc.edu>
2025-02-24 17:25:59 -0800

    working sqlite prototype

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -5,7 +6,15 @@ from flask_migrate import Migrate
 from config import Config, DevConfig, TestConfig, ProductionConfig
 
 
-db = SQLAlchemy()
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
 migrate = Migrate()
 login_manager = LoginManager()
 
