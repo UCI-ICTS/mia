4e3a67996d866b2a871ddb722dc4602a22a5ee01
HadleyKing <charles.hadley.king@gmail.com>
2025-04-02 10:59:51 -0400

    Changes to be committed: 	modified:   client/src/pages/ConsentPage.js 	modified:   server/utils/db_reset.sh

--- server/utils/db_reset.sh
+++ server/utils/db_reset.sh
@@ -4,12 +4,15 @@
 echo "yes" | python manage.py flush
 
 # Load data from fixtures
-python manage.py loaddata config/fixtures/initial.json
+# python manage.py loaddata config/fixtures/initial.json
 # python manage.py loaddata config/fixtures/form.json
 # python manage.py loaddata config/fixtures/test_final.json
 # python manage.py loaddata config/fixtures/test.json
 # python manage.py loaddata config/fixtures/family.json
 # python manage.py loaddata config/fixtures/dump.json
 # python manage.py loaddata config/fixtures/U09_dump.json
-# python manage.py loaddata tests/fixtures/initial.json
-# python manage.py loaddata tests/fixtures/test_fixture.json
+
+# python manage.py loaddata test/fixtures/family.json
+# python manage.py loaddata test/fixtures/first_branch.json
+# python manage.py loaddata test/fixtures/consent.json
+python manage.py loaddata test/fixtures/new_consent.json
