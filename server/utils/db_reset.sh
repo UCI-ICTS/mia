816fba82c535902a35f89f1fef7bb6aa9265a619
HadleyKing <charles.hadley.king@gmail.com>
2025-04-29 08:46:43 -0400

    Fix initial.json
    Changes to be committed:
    	modified:   server/config/fixtures/initial.json
    	modified:   server/utils/db_reset.sh

--- server/utils/db_reset.sh
+++ server/utils/db_reset.sh
@@ -4,7 +4,7 @@
 echo "yes" | python manage.py flush
 
 # Load data from fixtures
-# python manage.py loaddata config/fixtures/initial.json
+python manage.py loaddata config/fixtures/initial.json
 # python manage.py loaddata config/fixtures/start_consent.json
-python manage.py loaddata test/fixtures/family.json
+# python manage.py loaddata test/fixtures/family.json
 # python manage.py loaddata test/fixtures/new_consent.json
